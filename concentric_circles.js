0cd7196cd769f97891a5b82383a7ff3256e73d3b
mdevito12299 <43148221+mdevito12299@users.noreply.github.com>
2018-10-08 14:49:00 -0700

    Create concentric_circles.js

---
+++ concentric_circles.js
@@ -0,0 +1,16 @@
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(220);
+  noFill();
+  
+  
+for (var i = 100; i > 0; i = i -5) {
+ ellipse(200,200, i, i);
+
+  
+
+  }
+}
